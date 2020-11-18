6e8d6723d48c93823cc8a91c26bbf0b0ad5b9426
takatomo123456 <startikatika@gmail.com>
2020-11-18 15:00:16 +0900

    second commit

--- gitTestApp/ViewController.swift
+++ gitTestApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Tomoko-techより変更1回目")
+        print("takatomo123456より変更2回目")
     }
 
 
