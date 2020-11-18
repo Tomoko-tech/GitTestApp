81124ff5f780fcea85782b5e772233f3ef77186f
Tomoko-tech <tomoko.takahashi715@gmail.com>
2020-11-18 14:29:23 +0900

    first commit

---
+++ gitTestApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gitTestApp
+//
+//  Created by Takahashi Tomoko on 2020/11/18.
+//  Copyright © 2020 takatomo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Tomoko-techより変更1回目")
+    }
+
+
+}
+
